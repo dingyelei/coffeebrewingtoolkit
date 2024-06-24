344ed1d51f5de4e2cfa69c5e7ac86d8aab8d3609
Yelei Ding <yeleiding@Yeleis-Laptop.local>
2024-06-24 15:17:52 +0800

    remove license, so by default, this code should not be used for commercial purposes.

--- scripts/datavisulizer.py
+++ scripts/datavisulizer.py
@@ -28,13 +28,29 @@ class DataVisualizerMatplotlibAdapter(DataVisualizer):
 
     def brewing_chart(self, **param):
         print('build brewing chart')
-  
+        plt.style.use('_mpl-gallery')
+
+        # make the data
+        np.random.seed(3)
+        x = 4 + np.random.normal(0, 2, 8)
+        y = 4 + np.random.normal(0, 2, len(x))
+        # size and color:
+        sizes = np.random.uniform(15, 80, len(x))
+        colors = self.render_color(len(x))
+        # plot
+        fig, ax = plt.subplots()
+
+        ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
+        ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+            ylim=(0, 8), yticks=np.arange(1, 8))
+        plt.show()
+
     def render_color(self, size):
         np.random.seed(seed=int(time.time()))
         rgb = np.random.uniform(0, 1, 3)
         color_change = lambda x: x + 0.125 if x + 0.125 < 1 else x + 0.125 - 1
         colors = []
         for i in range(size):
-            rgb = [color_change(rgb[0]), rgb[1], rgb[2]]
+            rgb = [rgb[0], color_change(rgb[1]), rgb[2]]
             colors.append(rgb)
         return colors     
