791f7a02852acc67efb89e4fcf5ed0b319f996e6
Yelei Ding <yeleiding@Yeleis-Laptop.local>
2024-06-24 15:10:15 +0800

    delete unrelated file

--- scripts/datavisulizer.py
+++ scripts/datavisulizer.py
@@ -27,25 +27,8 @@ class DataVisualizerMatplotlibAdapter(DataVisualizer):
         self.name = "Matplotlib Data Visualizer"
 
     def brewing_chart(self, **param):
-
         print('build brewing chart')
-        plt.style.use('_mpl-gallery')
-
-        # make the data
-        np.random.seed(3)
-        x = 4 + np.random.normal(0, 2, 8)
-        y = 4 + np.random.normal(0, 2, len(x))
-        # size and color:
-        sizes = np.random.uniform(15, 80, len(x))
-        colors = self.render_color(len(x))
-        # plot
-        fig, ax = plt.subplots()
-
-        ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
-        ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-            ylim=(0, 8), yticks=np.arange(1, 8))
-        plt.show()
-
+  
     def render_color(self, size):
         np.random.seed(seed=int(time.time()))
         rgb = np.random.uniform(0, 1, 3)
